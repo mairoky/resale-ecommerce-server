7ef3e34818205deb6684e24385a3c3ecb78b551d
Aminul Islam Rocky <mairoky@outlook.com>
2022-11-26 13:28:32 +0600

    db connection & insert user data

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
+// TODO: JWT
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,6 +15,30 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Server is Running!')
 })
+
+// MongoDB Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v3j0rcs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// console.log(uri);
+
+async function run() {
+    try {
+        const database = client.db('resale_ecommerc');
+        const usersCollection = database.collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 app.listen(port, () => {
     console.log(`Server is running on PORT: ${port}`);
